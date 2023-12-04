ef8395b2c7a7278fbae4c4ccbc573a01c6879eb0
Saurabh Shankariya <sshankariya@gmail.com>
2023-12-04 15:22:25 +0530

    updates

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,16 +2,18 @@ import React from "react";
 import { INotedWithTags } from "../interface/INoteData";
 import { Row, Col } from "react-bootstrap";
 import NoteCard from "../components/NoteCard";
+import { useTranslation } from "react-i18next";
 
 type NoteWithTagsProps = {
   noteWithTags: INotedWithTags[];
   onDeleteNote: (id: string) => void;
-};
+}
 
 const Home: React.FunctionComponent<NoteWithTagsProps> = ({
   noteWithTags,
   onDeleteNote,
 }) : React.JSX.Element => {
+  const {t} = useTranslation();
   return (
     <div>
       <Row xs={1} sm={2} lg={3} xl={3} className="g-3 justify-content-space">
@@ -26,7 +28,7 @@ const Home: React.FunctionComponent<NoteWithTagsProps> = ({
       {noteWithTags.length == 0 && (
         <Row className="g-3 justify-content-space">
           <Col className="text-center">
-            <p className="fs-3">No record found</p>
+            <p className="fs-3">{t('No record found')}</p>
           </Col>
         </Row>
       )}
